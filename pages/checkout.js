701290402c4ebadce4f6421342e5eac24b9fe0aa
Alvarg27 <alvargbesne@gmail.com>
2022-03-09 19:01:49 -0600

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CheckoutPage from "../components/CheckoutPage";
 import MobileOrderSummary from "../components/MobileOrderSummary";
 
