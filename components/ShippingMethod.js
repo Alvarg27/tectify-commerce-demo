d8db617239d8fa5753e26b4b1a802d577d7e2e1c
Alvarg27 <alvargbesne@gmail.com>
2022-03-12 05:51:46 -0600

    Update ShippingMethod.js

--- components/ShippingMethod.js
+++ components/ShippingMethod.js
@@ -54,6 +54,7 @@ export default function ShippingMethod({ step, fetchCart, setStep, cart }) {
     }
     fetchCart();
     fetchShippingMethods();
+    setError(null);
   }, [step]);
 
   useEffect(() => {
